03e640f8780739e7d19f19ccd5e7a75f13bc1dcf
ChaemyungLim <limchaemyung1@gmail.com>
2024-07-21 14:17:49 +0900

    updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from typing import Dict, Any
+from typing import Dict, Any, List
 import yfinance as yf
 import json
 
 app = FastAPI()
 
 
-def get_yf_stockprice(stock_code: str) -> Dict[str, Any]:
+def get_yf_stockprice(stock_code: str) -> List[Dict[str, Any]]:
     ticker = yf.Ticker(f'{stock_code}.KS')
     df = ticker.history(
         interval='1d',
@@ -15,7 +15,21 @@ def get_yf_stockprice(stock_code: str) -> Dict[str, Any]:
         actions=True,
         auto_adjust=True
     )
-    return df.to_json(orient='index', date_format='iso')
+    df.reset_index(inplace=True)
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    result = []
+    for index, row in df.iterrows():
+        result.append({
+            'date': row['Date'],
+            'Open': row['Open'],
+            'High': row['High'],
+            'Low': row['Low'],
+            'Close': row['Close'],
+            'Volume': row['Volume'],
+            'Dividends': row['Dividends'],
+            'Stock Splits': row['Stock Splits']
+        })
+    return result
 
 
 @app.get("/stockprice")
@@ -25,7 +39,7 @@ def stockprice(stock_code: str):
     
     try:
         data = get_yf_stockprice(stock_code)
-        return json.loads(data)
+        return {"items": data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
